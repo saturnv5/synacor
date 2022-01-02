bc2dcbf34e52a974078bf51733c0cdcfcf512d2e
David X. Wang <xiao.wang.saturnv@gmail.com>
2022-01-03 10:17:05 +1100

    More teleporter puzzle attempts, this time using an explicit stack instead of recursion.

--- src/com/dixie/synacor/TeleporterPuzzle.java
+++ src/com/dixie/synacor/TeleporterPuzzle.java
@@ -1,6 +1,7 @@
 package com.dixie.synacor;
 
 import java.awt.*;
+import java.util.ArrayDeque;
 import java.util.HashMap;
 
 public class TeleporterPuzzle {
@@ -91,7 +92,7 @@ public class TeleporterPuzzle {
 
   private final HashMap<Point, Integer> cache = new HashMap<>();
 
-  /** Same thing, but with memoization. */
+  /** Same as D, but with memoization. */
   public int methodE(int x, int y) {
     Point key = new Point(x, y);
     Integer ans = cache.get(key);
@@ -99,7 +100,7 @@ public class TeleporterPuzzle {
       return ans;
     }
     if (x == 0) {
-      ans = y + 1;
+      ans = (y + 1) % Machine.MODULUS;
       cache.put(key, ans);
       return ans;
     } else if (y == 0) {
@@ -112,16 +113,88 @@ public class TeleporterPuzzle {
     return ans;
   }
 
-  public static void main(String[] args) {
+  /** Abandoned attempt; stack overflow. */
+  public static void main1(String[] args) {
     // 5483: set r0 4
     // 5486: set r1 1
     // 5489: call 6027
     // 5491: eq r1 r0 6
     for (int r7 = 0; r7 < Machine.MODULUS; r7++) {
-      int output = new TeleporterPuzzle(r7).methodE(4, 1) % Machine.MODULUS;
+      int output = new TeleporterPuzzle(r7).methodE(4, 1);
       System.out.println(String.format("output = %d, when r7 = %d", output, r7));
       if (output == 6) {
         break;
+      }
+    }
+  }
+
+  /** Second abandoned attempt, but got some values.
+   * output = 1, when r7 = 0, x = 0, y = 0
+   * output = 2, when r7 = 0, x = 0, y = 1
+   * output = 1, when r7 = 0, x = 1, y = 0
+   * output = 2, when r7 = 0, x = 1, y = 1
+   * output = 1, when r7 = 0, x = 2, y = 0
+   * output = 2, when r7 = 0, x = 2, y = 1
+   * output = 1, when r7 = 0, x = 3, y = 0
+   * output = 2, when r7 = 0, x = 3, y = 1
+   * output = 1, when r7 = 0, x = 4, y = 0
+   * output = 2, when r7 = 0, x = 4, y = 1
+   * output = 1, when r7 = 1, x = 0, y = 0
+   * output = 2, when r7 = 1, x = 0, y = 1
+   * output = 2, when r7 = 1, x = 1, y = 0
+   * output = 3, when r7 = 1, x = 1, y = 1
+   * output = 3, when r7 = 1, x = 2, y = 0
+   * output = 5, when r7 = 1, x = 2, y = 1
+   * output = 5, when r7 = 1, x = 3, y = 0
+   * output = 13, when r7 = 1, x = 3, y = 1
+   * output = 13, when r7 = 1, x = 4, y = 0
+   *
+   * 'y' doesn't seem to matter.
+   */
+  public static void main2(String[] args) {
+    for (int r7 = 0; r7 < Machine.MODULUS; r7++) {
+      for (int x = 0; x <= 4; x++) {
+        for (int y = 0; y <= 1; y++) {
+          int output = new TeleporterPuzzle(r7).methodE(x, y);
+          System.out.println(
+                  String.format("output = %d, when r7 = %d, x = %d, y = %d", output, r7, x, y));
+        }
+      }
+    }
+  }
+
+  /** Same as E, but with an explicit stack instead of recursion. */
+  private int methodF(int x, int y) {
+    ArrayDeque<Integer> stack = new ArrayDeque<>();
+    stack.push(x);
+    while (!stack.isEmpty()) {
+      x = stack.pop();
+      if (x == 0) { // base-case
+        y = (y + 1) % Machine.MODULUS;
+      } else if (y == 0) { // tail-recursion
+        y = r7;
+        stack.push(x - 1);
+      } else { // nested-recursion
+        y--;
+        stack.push(x - 1);
+        stack.push(x); // This will naturally set 'y' to the correct value for the line above.
+      }
+    }
+    return y;
+  }
+
+  /**
+   * A bit better, I now get an answer for r7 = 1 (32765), r7 = 2 (13234), but r3 takes too long.
+   */
+  public static void main3(String[] args) {
+    for (int r7 = 1; r7 < Machine.MODULUS; r7++) {
+      for (int x = 0; x <= 4; x++) {
+        int output = new TeleporterPuzzle(r7).methodF(x, 1);
+        System.out.println(
+                String.format("output = %d, when r7 = %d, x = %d, y = 1", output, r7, x));
+        if (output == 6) {
+          break;
+        }
       }
     }
   }
